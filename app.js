82715a8e0abff0a293c404143ac2ad9b96b18de9
natia <90927569+natiasha@users.noreply.github.com>
2021-10-21 17:44:25 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let x=7;
+let y=5;
+let s=x*y;
+console.log(s);
+let a=5;
+let b=6;
+let c=6;
+let p=(a+b+c)/2;
+let m=Math.sqrt(p*(p-a)*(p-b)*(p-c));
+console.log(m);
+let r=7;
+let n=Math.PI * r*r;
+console.log(n);
+//boolean
+17<5
+console.log(17<5)
